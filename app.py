7c29292bdf2451150e9773556e0d1c3aec1e6bb1
aisudev <tk.thanatorn@gmail.com>
2022-02-17 09:41:08 +0700

    update

--- app.py
+++ app.py
@@ -1,9 +1,19 @@
 from routes import torch, tf
 import fastapi
 import uvicorn
+from fastapi.middleware import cors
 
 # Init Main Router
 app = fastapi.FastAPI()
+# Setup CORS
+origins = ['*']
+app.add_middleware(
+    cors.CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 # Add sub router (pytorch)
 app.include_router(torch.router, prefix='/torch', tags=['model'])
 # Add sub router (tensorflow)
